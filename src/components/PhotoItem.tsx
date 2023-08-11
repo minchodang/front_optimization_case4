f7afc895803d4d9b613284ff07575cf910f5dbc0
minsuKang <minsu910725@gmail.com>
2023-08-11 09:37:47 +0900

    Fix: image 지연 로딩 및 cls 최적화 작업

--- src/components/PhotoItem.tsx
+++ src/components/PhotoItem.tsx
@@ -1,4 +1,5 @@
 import { PhotoType } from '@/redux/photos';
+import LazyLoad from 'react-lazyload';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { showModal } from '../redux/imageModal';
@@ -16,16 +17,26 @@ function PhotoItem({ photo: { urls, alt } }: Props) {
 
   return (
     <ImageWrap>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <LazyLoad offset={1000}>
+        <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      </LazyLoad>
     </ImageWrap>
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
